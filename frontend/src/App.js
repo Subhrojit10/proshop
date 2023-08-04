b2387b81ef517e723acef3235f805d008ee03059
Subho45 <mightysubho@gmail.com>
2023-08-04 18:52:23 +0530

    change router from app to index.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,10 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Outlet,
+} from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
@@ -7,19 +12,15 @@ import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
 const App = () => {
   return (
-    <Router>
+    <>
       <Header />
       <main className='py-3'>
         <Container>
-          <Routes>
-            <Route path='/' element={<HomeScreen />} exact />
-            <Route path='/product/:id' element={<ProductScreen />}  />
-
-          </Routes>
+          <Outlet />
         </Container>
       </main>
       <Footer />
-    </Router>
+    </>
   )
 }
 
