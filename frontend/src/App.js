7e180ab3e2bbf3c90d4676802f2bd1824a316ae5
Subho45 <mightysubho@gmail.com>
2023-08-04 18:59:34 +0530

    app.js code clean up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,8 @@
 import React from 'react'
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Outlet,
-} from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
-import HomeScreen from './screens/HomeScreen'
-import ProductScreen from './screens/ProductScreen'
 const App = () => {
   return (
     <>
