41927754411bc660b3cd36b3fda57a855419a269
Subho45 <mightysubho@gmail.com>
2023-08-04 16:31:12 +0530

    error handler added

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -11,7 +11,6 @@ router.get(
   '/',
   asyncHandler(async (req, res) => {
     const products = await Product.find({})
-
     res.json(products)
   })
 )
@@ -24,13 +23,12 @@ router.get(
   '/:id',
   asyncHandler(async (req, res) => {
     const product = await Product.findById(req.params.id)
-    
     if (product) {
       return res.json(product)
-    } 
-
-     res.status(404).json({ message: 'Product not found' })
-    
+    } else {
+      res.status(404)
+      throw new Error('Resource not found')
+    }
   })
 )
 
