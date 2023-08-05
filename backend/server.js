3e147b92d8a7e0689328935e9f7e12537d8cc154
Subho45 <mightysubho@gmail.com>
2023-08-05 16:43:04 +0530

    user routes and controlers created

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,9 @@ import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
 import connectDB from './config/db.js'
-import { notFound,errorHandler } from './middleware/errorMiddleware.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 
 dotenv.config()
 
@@ -15,7 +16,8 @@ app.get('/', (req, res) => {
   res.send('API is running....')
 })
 
-app.use('/api/products',productRoutes)
+app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 app.use(notFound)
 app.use(errorHandler)
@@ -23,5 +25,7 @@ app.use(errorHandler)
 const PORT = process.env.PORT || 5000
 app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 )
